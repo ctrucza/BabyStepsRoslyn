e78d4c0a7172b6e7d0140436c7b3c052dc774892
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-09-25 17:29:41 +0300

    Sample code to count classes

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using Microsoft.CodeAnalysis.MSBuild;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
 
 namespace HelloWorld
 {
@@ -9,8 +11,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            OpenSolution();
-            OpenProject();
+            CountClasses();
         }
 
         private static void OpenSolution()
@@ -26,7 +27,7 @@ namespace HelloWorld
         {
             var workspace = MSBuildWorkspace.Create();
             var project = workspace.OpenProjectAsync(@"HelloWorld\HelloWorld.csproj").Result;
-            Console.WriteLine(project.FilePath);
+            Console.WriteLine("Project file: " + project.FilePath);
             Console.WriteLine("{0} Documents:", project.Documents.Count());
             project.Documents.ToList().ForEach(document => Console.WriteLine("{0} ({1})", document.Name, document.FilePath));
             project.Documents.ToList().ForEach(document =>
@@ -40,6 +41,42 @@ namespace HelloWorld
             {
                 var root = syntaxTree.GetRoot();
             });
+        }
+
+        private static void CountClasses()
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var project = workspace.OpenProjectAsync(@"Sample\Sample.csproj").Result;
+            //Console.WriteLine("Project file: " + project.FilePath);
+            //Console.WriteLine("{0} Documents:", project.Documents.Count());
+            //project.Documents.ToList().ForEach(document => Console.WriteLine("{0} ({1})", document.Name, document.FilePath));
+
+            List<ClassDeclarationSyntax> classes = new List<ClassDeclarationSyntax>();
+
+            project.Documents.ToList().ForEach(document =>
+            {
+                var syntaxTree = document.GetSyntaxTreeAsync().Result;
+                var semanticModel = document.GetSemanticModelAsync().Result;
+                var root = document.GetSyntaxRootAsync().Result;
+                //Console.WriteLine("Classes in {0}: ", document.Name );
+                var classesInDocument = root.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+                //classesInDocument.ForEach(c => Console.WriteLine(c.Identifier));
+                //Console.WriteLine("{0} classes found", classesInDocument.Count);
+                classes.AddRange(classesInDocument);
+            });
+
+            var compilation = project.GetCompilationAsync().Result;
+            compilation.SyntaxTrees.ToList().ForEach(syntaxTree =>
+            {
+                var root = (CompilationUnitSyntax)syntaxTree.GetRoot();
+                //Console.WriteLine("Classes in {0}:", syntaxTree.FilePath);
+                var classesInSyntaxTree = root.DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+                //classesInSyntaxTree.ForEach(c=>Console.WriteLine(c.Identifier));
+                //Console.WriteLine("{0} classes found", classesInSyntaxTree.Count);
+            });
+
+
+            classes.ForEach(c => Console.WriteLine(c.Identifier));
         }
 
     }
