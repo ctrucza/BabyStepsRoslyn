2737146cbc0e95fa68640869aa714f5530efc362
Csaba Trucza <csaba.trucza@iquestgroup.com>
2014-09-25 17:09:35 +0300

    Example for opening a solution or a projects

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using Microsoft.CodeAnalysis.MSBuild;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OpenSolution();
+            OpenProject();
+        }
+
+        private static void OpenSolution()
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var solution = workspace.OpenSolutionAsync(@"BabyStepsRoslyn.sln").Result;
+            Console.WriteLine(solution.FilePath);
+            Console.WriteLine("{0} Projects:", solution.Projects.Count());
+            solution.Projects.ToList().ForEach(project => Console.WriteLine(project.Name));
+        }
+
+        private static void OpenProject()
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var project = workspace.OpenProjectAsync(@"HelloWorld\HelloWorld.csproj").Result;
+            Console.WriteLine(project.FilePath);
+            Console.WriteLine("{0} Documents:", project.Documents.Count());
+            project.Documents.ToList().ForEach(document => Console.WriteLine("{0} ({1})", document.Name, document.FilePath));
+            project.Documents.ToList().ForEach(document =>
+            {
+                var syntaxTree = document.GetSyntaxTreeAsync().Result;
+                var semanticModel = document.GetSemanticModelAsync().Result;
+                var root = document.GetSyntaxRootAsync().Result;
+            });
+            var compilation = project.GetCompilationAsync().Result;
+            compilation.SyntaxTrees.ToList().ForEach(syntaxTree =>
+            {
+                var root = syntaxTree.GetRoot();
+            });
+        }
+
+    }
+}
