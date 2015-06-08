101279ce166ff0d642664f9b43b50221eaece3d2
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-06-08 10:53:22 +0300

    Trying to get nested namespaces and inner classes correctly

--- MethodLength/Program.cs
+++ MethodLength/Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.MSBuild;
 
@@ -14,7 +9,8 @@ namespace MethodLength
     {
         static void Main(string[] args)
         {
-            string solutionFilePath = @"H:\Battleship\Battleship.sln";
+            //string solutionFilePath = @"H:\Battleship\Battleship.sln";
+            string solutionFilePath = @"C:\Users\csaba.trucza\Documents\Hacks\BabyStepsRoslyn\BabyStepsRoslyn.sln";
             var workspace = MSBuildWorkspace.Create();
             var solution = workspace.OpenSolutionAsync(solutionFilePath).Result;
             foreach (var project in solution.Projects)
@@ -22,20 +18,49 @@ namespace MethodLength
                 foreach (var document in project.Documents)
                 {
                     var root = document.GetSyntaxRootAsync().Result;
-                    foreach (var method in root.DescendantNodes().OfType<MethodDeclarationSyntax>())
+                    foreach (var namespaceDeclaration in root.DescendantNodes().OfType<NamespaceDeclarationSyntax>())
                     {
-                        var c = GetClass(method);                        
-                        var totalLines = method.GetText().Lines.Count;
-                        var bodyLines = method.Body.GetText().Lines.Count;
-
-                        var statementsInBody = method.Body.ChildNodes().OfType<StatementSyntax>().Count();
-                        var totalStatementCount = method.Body.DescendantNodes().OfType<StatementSyntax>().Count();
-
-                        var statementLines = method.Body.Statements.Sum(s=>s.GetText().Lines.Count);
-                        Console.WriteLine("{0}\t{1}\t{2}\t{3}", totalStatementCount, statementsInBody, c.Identifier, method.Identifier);
-                        //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}", c.Identifier, method.Identifier, totalLines, bodyLines, statementsInBody, directStatementCount, totalStatementCount, statementLines);
+                        foreach (var classDeclaration in namespaceDeclaration.DescendantNodes().OfType<ClassDeclarationSyntax>())
+                        {
+                            CalculateMethodLengthsInClass(classDeclaration, namespaceDeclaration.Name.ToString());
+                            CalculateMethodLengthsInInnerClasses(classDeclaration, namespaceDeclaration.Name.ToString());
+                        }
                     }
+                    
+
+                    //foreach (var method in root.DescendantNodes().OfType<MethodDeclarationSyntax>())
+                    //{
+                    //    var c = GetClass(method);                        
+                    //    var totalLines = method.GetText().Lines.Count;
+                    //    var bodyLines = method.Body.GetText().Lines.Count;
+
+                    //    var statementsInBody = method.Body.ChildNodes().OfType<StatementSyntax>().Count();
+                    //    var totalStatementCount = method.Body.DescendantNodes().OfType<StatementSyntax>().Count();
+
+                    //    var statementLines = method.Body.Statements.Sum(s=>s.GetText().Lines.Count);
+                    //    Console.WriteLine("{0}\t{1}\t{2}\t{3}", totalStatementCount, statementsInBody, c.Identifier, method.Identifier);
+                    //    //Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}", c.Identifier, method.Identifier, totalLines, bodyLines, statementsInBody, directStatementCount, totalStatementCount, statementLines);
+                    //}
                 }
+            }
+        }
+
+        private static void CalculateMethodLengthsInInnerClasses(ClassDeclarationSyntax classDeclaration, string containerName)
+        {
+            foreach (var innerClassDeclaration in classDeclaration.DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                var s = string.Format("{0}.{1}", containerName, innerClassDeclaration.Identifier);
+                CalculateMethodLengthsInClass(innerClassDeclaration, s);
+                CalculateMethodLengthsInInnerClasses(innerClassDeclaration, s);
+            }
+        }
+
+        private static void CalculateMethodLengthsInClass(ClassDeclarationSyntax classDeclaration, string containerName)
+        {
+            foreach (var methodDeclaration in classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>())
+            {
+                var methodContainerName = string.Format("{0}.{1}", containerName, classDeclaration.Identifier); 
+                Console.WriteLine("{0}.{1}", methodContainerName, methodDeclaration.Identifier);
             }
         }
 
--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,7 +1,37 @@
 ﻿namespace Sample
 {
+    namespace Hoppa
+    {
+        class Program
+        {
+        }
+
+        namespace Troppa
+        {
+            class Program
+            {
+                
+            }
+        }
+    }
+
     class Program
     {
+        class InnerClass
+        {
+            class EvenMoreInnerClass
+            {
+                public void MethodInEvenMoreInnerClass()
+                {
+                    
+                }
+            }
+
+            public void MethodInInnerClass()
+            {
+                
+            }
+        }
         static void Main(string[] args)
         {
         }
