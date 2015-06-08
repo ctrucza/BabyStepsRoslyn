913a0795e9b1b32fc351599390c5f09bd391386d
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-06-08 11:04:46 +0300

    Added crazy namespace and class hierarcy

--- SampleSolution/Program.cs
+++ SampleSolution/Program.cs
@@ -6,6 +6,64 @@ using System.Threading.Tasks;
 
 namespace SampleSolution
 {
+    namespace NestedNamespace
+    {
+        namespace DoublyNestedNamespace
+        {
+            namespace NamespaceThreeDeep
+            {
+                class VeryDeepClass
+                {
+                    class VeryDeepInnerClass
+                    {
+                        class MariannaTrench
+                        {
+                            
+                        }
+                    }
+                }        
+            }
+        
+        }
+
+        class ClassInNestedNamespace
+        {
+            
+        }
+
+        namespace AnotherDoublyNestedNamespace
+        {
+        
+        }
+    }
+
+    namespace AnotherNestedNamespace
+    {
+        class ClassInAnotherNestedNamespace
+        {
+            
+        }
+    }
+
+    class OuterClass
+    {
+        class InnerClass
+        {
+            class InnerClassInInnerClass
+            {
+                class ClassThreeDeep
+                {
+                    
+                }
+            }
+        }
+
+        class AnotherInnerClass
+        {
+            
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
